154e42a0186e8c77014fbc984acbf71043063343
rahmaAlmamari <alr816125@gmail.com>
2025-07-08 16:31:19 +0400

    to display the values of the user object

--- Program.cs
+++ Program.cs
@@ -8,10 +8,18 @@
             //Create an instance of the UserTest class
             UserTest user = new UserTest();
 
-            //enter a value to user object fields which is a copye of UserTest class
-            user.UserName = "Rahma Almamari";
-            user.Email = "rahma.almamari@gmail.com";
-            user.Password = "12345678";
+            ////enter a value to user object fields which is a copye of UserTest class
+            //user.UserName = "Rahma Almamari";
+            //user.Email = "rahma.almamari@gmail.com";
+            //user.Password = "12345678";
+
+            Console.WriteLine("-----------------------------------------------");
+
+            //to display the values of the user object
+            Console.WriteLine("User Details:");
+            Console.WriteLine("User Name: " + user.UserName);
+            Console.WriteLine("Email: " + user.Email);
+            Console.WriteLine("Password: " + user.Password);
         }
     }
 }
