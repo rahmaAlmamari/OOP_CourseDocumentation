62f9457ebde969102d1cc681698954fec34e865d
rahmaAlmamari <alr816125@gmail.com>
2025-07-08 14:06:10 +0400

    enter a value to user object fields which is a copye of UserTest class

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@
 
             //Create an instance of the UserTest class
             UserTest user = new UserTest();
+
+            //enter a value to user object fields which is a copye of UserTest class
+            user.UserName = "Rahma Almamari";
+            user.Email = "rahma.almamari@gmail.com";
+            user.Password = "12345678";
         }
     }
 }
