59ea76820e748737ba3ea1358e99314f7b1ba941
rahmaAlmamari <alr816125@gmail.com>
2025-07-09 09:31:08 +0400

    Adding ViewUserDetails method to UserTest class

--- UserTest.cs
+++ UserTest.cs
@@ -13,7 +13,7 @@ namespace OOP_CourseDocumentation
         public string UserName;
         public string Password;
         public string Email;
-        private double Balance;
+        private double Balance = 100;
 
         //Static class members
         public static int UserCount = 0;
@@ -27,11 +27,21 @@ namespace OOP_CourseDocumentation
             Password = password;
             UserCount++;
             Console.WriteLine("User created successfully!");
-            //to display the values of the user object
-            Console.WriteLine("User Details:");
-            Console.WriteLine("User Name: " + userName);
-            Console.WriteLine("Email: " + email);
-            Console.WriteLine("Password: " + password);
+           
+        }
+        public void ViewUserDetails(string password)
+        {
+            //to check if the user is valid or not ...
+            if(password == Password)
+            {
+                //to display the values of the user object
+                Console.WriteLine("User Details:");
+                Console.WriteLine("User Name: " + UserName);
+                Console.WriteLine("Email: " + Password);
+                Console.WriteLine("Password: " + Email);
+                Console.WriteLine("Balance:" + Balance);
+            }
+            
         }
 
 
