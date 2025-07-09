050c9e97392ba1ed0eac08194dde2da81e62ec6e
rahmaAlmamari <alr816125@gmail.com>
2025-07-09 08:57:32 +0400

    Modify switch logic

--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
 
-            ////Create an instance of the UserTest class
-            //UserTest user = new UserTest();
+            //Create an instance of the UserTest class
+            UserTest user = new UserTest();
 
             //to show user menu options
             Console.WriteLine("Welcome to User Management System");
@@ -18,30 +18,39 @@
             Console.WriteLine("5. Exit");
 
             char userChoice = '0'; // Initialize userChoice to avoid uninitialized variable warning
+            userChoice = Console.ReadKey().KeyChar; // Read user input
             switch (userChoice)
             {
                 case '1':
-                    Console.WriteLine("Creating User...");
+                    //Console.WriteLine("Creating User...");
                     // Code to create user
+                    string userName, email, password;
+                    Console.Write("Enter User Name: ");
+                    userName = Console.ReadLine();
+                    Console.Write("Enter Email: ");
+                    email = Console.ReadLine();
+                    Console.Write("Enter Password: ");
+                    password = Console.ReadLine();
+                    user.CreateUser(userName, email, password);
                     break;
                 case '2':
-                    Console.WriteLine("Viewing User Details...");
+                    //Console.WriteLine("Viewing User Details...");
                     // Code to view user details
                     break;
                 case '3':
-                    Console.WriteLine("Checking Balance...");
+                    //Console.WriteLine("Checking Balance...");
                     // Code to check balance
                     break;
                 case '4':
-                    Console.WriteLine("Depositing...");
+                    //Console.WriteLine("Depositing...");
                     // Code to deposit
                     break;
                 case '5':
-                    Console.WriteLine("Exiting...");
+                    //Console.WriteLine("Exiting...");
                     // Code to exit
                     break;
                 default:
-                    Console.WriteLine("Invalid choice. Please try again.");
+                    //Console.WriteLine("Invalid choice. Please try again.");
                     break;
             }
 
@@ -60,11 +69,11 @@
 
             Console.WriteLine("-----------------------------------------------");
 
-            //to display the values of the user object
-            Console.WriteLine("User Details:");
-            Console.WriteLine("User Name: " + user.UserName);
-            Console.WriteLine("Email: " + user.Email);
-            Console.WriteLine("Password: " + user.Password);
+            ////to display the values of the user object
+            //Console.WriteLine("User Details:");
+            //Console.WriteLine("User Name: " + user.UserName);
+            //Console.WriteLine("Email: " + user.Email);
+            //Console.WriteLine("Password: " + user.Password);
         }
     }
 }
