7a8b92ebcd3eea8dbfca12b2b4a45b4ec18aa574
rahmaAlmamari <alr816125@gmail.com>
2025-07-09 08:45:27 +0400

    Adding CreateUser method in UserTest class

--- UserTest.cs
+++ UserTest.cs
@@ -19,6 +19,28 @@ namespace OOP_CourseDocumentation
         public static int UserCount = 0;
 
 
+        //Create an instance of the UserTest class
+        UserTest user = new UserTest(); 
+
+        //UserTest Class Methods ...
+        //to create new user ...
+        public void CreateUser()
+        {
+
+            //to get the values of the user object from the user input
+            string userName;
+            string email;
+            string password;
+
+            Console.Write("Enter User Name: ");
+            user.UserName = Console.ReadLine();
+            Console.Write("Enter Email: ");
+            user.Email = Console.ReadLine();
+            Console.Write("Enter Password: ");
+            user.Password = Console.ReadLine();
+            UserCount++;
+            Console.WriteLine("User created successfully!");
+        }
 
 
     }
